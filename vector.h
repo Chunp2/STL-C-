897fcf730697d3085b3efc9b397f8761b3ab1e60
Paul Chun <chunpaul000@gmail.com>
2019-10-04 10:50:31 -0700

    Add files via upload

---
+++ vector.h
@@ -0,0 +1,60 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+#include <iostream>
+
+using namespace std;
+
+class vector{
+	vector();
+	~vector();
+
+	// Iterators
+	/*begin();
+	end();
+	rbegin();
+	rend();
+	cbegin();
+	cend();
+	crbegin();
+	crend();
+
+	// Capacity
+	size();
+	max_size();
+	resize();
+	capacity();
+	empty();
+	reserve();
+	shrink_to_fit();
+
+	// Element access
+	at();
+	front();
+	back();
+	data();
+
+	// Modifieres
+	assign();
+	push_back();
+	pop_back();
+	insert();
+	erase();
+	swap();
+	clear();
+	emplace();
+	emplace_back();
+
+	// Allocator
+	get_allocator();
+
+	// Non-member function overloads
+	relational_operators();
+	swap();
+
+	// Template specializaitons
+	vector<bool>;
+*/
+};
+
+#endif
